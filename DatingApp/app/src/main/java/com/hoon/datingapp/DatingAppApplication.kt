ff0aed661c35f974c814e5d97e0ce3df23813160
tkdgns8234 <55433939+tkdgns8234@users.noreply.github.com>
2022-09-03 11:25:12 +0900

    데이팅 앱 V2.2.1

--- DatingApp/app/src/main/java/com/hoon/datingapp/DatingAppApplication.kt
+++ DatingApp/app/src/main/java/com/hoon/datingapp/DatingAppApplication.kt
@@ -1,6 +1,7 @@
 package com.hoon.datingapp
 
 import android.app.Application
+import android.os.Build
 import com.hoon.datingapp.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -12,7 +13,11 @@ class DatingAppApplication : Application() {
         super.onCreate()
 
         startKoin {
-            androidLogger(Level.ERROR) // koin의 로그 레벨을 지정
+            if (BuildConfig.DEBUG) {
+                androidLogger(Level.ERROR) // koin의 로그 레벨을 지정
+            } else {
+                androidLogger(Level.NONE)
+            }
             androidContext(this@DatingAppApplication) // context 등록
             modules(appModule)
         }
